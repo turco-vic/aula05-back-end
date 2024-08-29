60f69a89d6088fb49397c27bb789eeda454e7159
Enzo Turcovic <enzoturcovic@gmail.com>
2024-08-29 15:51:21 -0300

    feat: some requests

--- src/server.js
+++ src/server.js
@@ -8,10 +8,57 @@ const serverPort = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+const guloseimas = [
+  {
+    id: 1,
+    nome: "Trufa",
+    preco: 7.77
+  },
+  {
+    id: 2,
+    nome: "Gud-gud",
+    preco: 10.50
+  },
+  {
+    id: 3,
+    nome: "Geleia-caioba",
+    preco: 5
+  },
+];
+
+const filmes = [
+  {
+    id: 1002,
+    titulo: "Meu irmão urso",
+    genero: "Animação",
+    emCartaz: false
+  },
+  {
+    id: 1003,
+    titulo: "DeadPool and Wolverine",
+    genero: "Ação",
+    emCartaz: true,
+  },
+  {
+    id: 1004,
+    titulo: "É assim que acaba",
+    genero: "Romance",
+    emCartaz: true,
+  }
+]
+
+
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Hello, World!" });
 });
+app.get("/doces", (req, res) => {
+  return res.status(200).json(guloseimas);
+});
+
+app.get("/filmes", (req, res) => {
+  return res.status(200).json(filmes);
+});
 
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
+  console.log(`💖💋 Server started on http://localhost:${serverPort}💖💋`);
 });
